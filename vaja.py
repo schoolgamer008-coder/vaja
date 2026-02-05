46b66029a93c685f311f558dac54683768ef9c08
Peter <schoolgamer008@gmail.com>
2026-02-05 19:13:13 +0100

    first commit

---
+++ vaja.py
@@ -0,0 +1,18 @@
+#Napiši snake-game v pygamu
+#koda od prej ti lahko sliži za inspiracijo (npr dolzina kace s kvadratki -> to pride prav)
+
+#za projekt naredite github repo, in spremembe sproti comitatje addajte in pushajte
+#na koncu morajo biti v repozitoriju vsaj 3 vecji commiti
+
+#1. dodatna naloga:
+#naredi branch "izgled"
+#v tem brancu naredi logiko, da ko igra tece, lahko pritisnes gumb "space" kar celotni kaci nastavi nakljucno barvo
+
+#2. dodatna naloga:
+#naredi branch "multiplayer"
+#v tem branchu naredi logiko, da sta na zacetku igre 2 kaci, ena se upravlja z wasd, druga z gumbi s puscicami
+#ce se aca zabije vase, v drugo kaco ali v steno, izgubi
+
+#3. dodatna naloga
+#naredi megre obeh branchov 
+
