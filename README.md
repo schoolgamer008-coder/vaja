9f09328d796808f137a5c5c85692ac0da5ca5ec5
Peter <schoolgamer008@gmail.com>
2026-02-05 19:35:21 +0100

    basic pygame window

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# vaja
-vaja
+snake game in pygame
--- vaja.py
+++ vaja.py
@@ -1,18 +1,28 @@
-#Napiši snake-game v pygamu
-#koda od prej ti lahko sliži za inspiracijo (npr dolzina kace s kvadratki -> to pride prav)
+# snake game
 
-#za projekt naredite github repo, in spremembe sproti comitatje addajte in pushajte
-#na koncu morajo biti v repozitoriju vsaj 3 vecji commiti
+import pygame
 
-#1. dodatna naloga:
-#naredi branch "izgled"
-#v tem brancu naredi logiko, da ko igra tece, lahko pritisnes gumb "space" kar celotni kaci nastavi nakljucno barvo
+pygame.init()
 
-#2. dodatna naloga:
-#naredi branch "multiplayer"
-#v tem branchu naredi logiko, da sta na zacetku igre 2 kaci, ena se upravlja z wasd, druga z gumbi s puscicami
-#ce se aca zabije vase, v drugo kaco ali v steno, izgubi
+WIDTH, HEIGHT = 1920, 1080
+canvas = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pygame")
 
-#3. dodatna naloga
-#naredi megre obeh branchov 
+exit_game = False
+clock = pygame.time.Clock()
 
+picture = pygame.image.load("pygameimage1.jpg")
+picture = pygame.transform.scale(picture, (WIDTH, HEIGHT))
+
+while not exit_game:
+    canvas.blit(picture, (0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit_game = True
+
+
+    pygame.display.update()
+    clock.tick(3600)
+
+pygame.quit()
