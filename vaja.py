bb58fae8f66868797348e77fd6415c4f0bed5c04
Peter <schoolgamer008@gmail.com>
2026-02-12 18:51:07 +0100

    dodou zaznavanje če se zadane v zid al pa vase

--- vaja.py
+++ vaja.py
@@ -38,7 +38,17 @@ def move_snake(x, y, direction):
     return x, y
 
 def check_collision(x, y, snake_list):
-    return None
+    if x<0 or x>= WIDTH:
+        return True
+    
+    if y<0 or y>= HEIGHT:
+        return True
+    
+    for block in snake_list[:-1]:
+        if block == [x,y]:
+            return True
+        
+    return False
 
 def food_position():
     return [random.randrange(0, WIDTH - BLOCK_SIZE, BLOCK_SIZE),
@@ -66,11 +76,11 @@ while not exit_game:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT and direction[0] == 0:
                 direction = [-BLOCK_SIZE, 0]
-            if event.key == pygame.K_LEFT and direction[0] == 0:
+            if event.key == pygame.K_RIGHT and direction[0] == 0:
                 direction = [BLOCK_SIZE, 0]
-            if event.key == pygame.K_LEFT and direction[0] == 0:
+            if event.key == pygame.K_UP and direction[1] == 0:
                 direction = [0, -BLOCK_SIZE]
-            if event.key == pygame.K_LEFT and direction[0] == 0:
+            if event.key == pygame.K_DOWN and direction[1] == 0:
                 direction = [0, BLOCK_SIZE]
 
     x,y = move_snake(x,y, direction)
@@ -87,6 +97,8 @@ while not exit_game:
         food_pos = food_position()
 
     snake_list.append([x, y])
+    if len(snake_list) > snake_length:
+        del snake_list[0]
 
     if [x, y] == food_pos:
         snake_length += 1
