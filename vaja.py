8fa21650ea4f310e23601164b42cc78e0305f0ff
Peter <schoolgamer008@gmail.com>
2026-03-05 18:12:39 +0100

    added branch look

--- vaja.py
+++ vaja.py
@@ -1,5 +1,18 @@
 # snake game
 
+"""
+#1. dodatna naloga:
+#naredi branch "izgled"
+#v tem brancu naredi logiko, da ko igra tece, lahko pritisnes gumb "space" kar celotni kaci nastavi nakljucno barvo
+
+#2. dodatna naloga:
+#naredi branch "multiplayer"
+#v tem branchu naredi logiko, da sta na zacetku igre 2 kaci, ena se upravlja z wasd, druga z gumbi s puscicami
+#ce se aca zabije vase, v drugo kaco ali v steno, izgubi
+
+#3. dodatna naloga
+#naredi megre obeh branchov 
+"""
 import pygame
 import random
 
